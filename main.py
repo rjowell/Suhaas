1a2c87b00eca698b326f19998c9c711b62adc2be
rjowell20007 <44178512+rjowell@users.noreply.github.com>
2022-01-29 02:36:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Welcome to Suhaas' Ice Cream Shop")
+print("What kind of ice cream do you want?")
+
+iceCreamType = input("A - Vanilla B - Cookies and Cream C - Chocolate")
+
+if iceCreamType == "A":
+  print("Here's your vanilla")
+elif iceCreamType == "B":
+  print("Here's Cookies and Cream")
+elif iceCreamType == "C":
+  print("Yummy chooclate")
+else:
+  print("we don't have that")
